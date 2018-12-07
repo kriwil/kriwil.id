547b88a80a512ff0cb4662b28acea22e392e0a9e
Aldiantoro Nugroho <kriwil@gmail.com>
2018-12-08 05:35:02 +0700

    use the same theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,15 +2,19 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'aldi'
-SITENAME = 'kriwil'
-SITEURL = ''
+AUTHOR = "aldi"
+SITENAME = "kriwil.id"
+SITEURL = ""
 
-PATH = 'content'
+PATH = "content"
 
-TIMEZONE = 'Asia/Jakarta'
+TIMEZONE = "Asia/Jakarta"
 
-DEFAULT_LANG = 'id'
+THEME = "themes/kriwil.id"
+DEFAULT_LANG = "id"
+
+PLUGIN_PATHS = ["plugins"]
+PLUGINS = ["assets", "custom_article_urls", "sitemap"]
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -20,12 +24,26 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('kriwil.com', 'https://kriwil.com/'),)
+LINKS = (("kriwil.com", "https://kriwil.com/"),)
 
 # Social widget
-SOCIAL = (('twitter', 'https://twitter.com/kriwil'),)
+SOCIAL = (("twitter", "https://twitter.com/kriwil"),)
 
-DEFAULT_PAGINATION = False
+DISPLAY_PAGES_ON_MENU = True
+DEFAULT_PAGINATION = 20
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
+
+SITEMAP = {
+    "format": "xml",
+    "changefreqs": {"articles": "daily", "pages": "daily", "indexes": "daily"},
+    "exclude": ["tag/", "category/"],
+}
+
+CUSTOM_ARTICLE_URLS = {
+    "links": {
+        "URL": "{category}/{date:%Y%m%d}/{slug}/",
+        "SAVE_AS": "{category}/{date:%Y%m%d}/{slug}/index.html",
+    }
+}
